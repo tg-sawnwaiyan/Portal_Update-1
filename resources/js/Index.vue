dd96e0405178bf2b5376c9d68fa3dbfa40e3dcbd
ThuzarTS <thuzar.ts92@gmail.com>
2019-12-02 16:52:57 +0630

    register editing pending

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -182,10 +182,31 @@
     }, 
 
     mounted(){
-        this.axios.get('auth/user').then(res=>{
-            console.log(res)
+        this.axios.get('/api/auth/user').then(res=>{
+            // console.log(res)
         })
+
+        var mybutton = document.getElementById("myBtn");
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+            function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
         }
+        // When the user clicks on the button, scroll to the top of the document
+        
+    },
+    method(){
+        topFunction = function () {
+            // document.body.scrollTop = 0;
+            // document.documentElement.scrollTop = 0;
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+	            return false;
+        }
+    }
 
    
   }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -159,23 +159,7 @@
  $(document).ready(function() {
    
      /*added for back to top*/
-     var mybutton = document.getElementById("myBtn");
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {scrollFunction()};
-            function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        }
-        // When the user clicks on the button, scroll to the top of the document
-        topFunction = function () {
-            // document.body.scrollTop = 0;
-            // document.documentElement.scrollTop = 0;
-            $("html, body").animate({ scrollTop: 0 }, "slow");
-	            return false;
-        }
+     
 
     if(localStorage.getItem("hospital_history")){
         $("#hos-his-local").html(localStorage.getItem("hospital_history").split(",").length);
