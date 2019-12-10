e41da4a0a9707827349ea9e3779a8a1bd5e1372c
MayPhueKyawSoe <mayphuekyawsoe123@gmail.com>
2019-12-10 13:27:48 +0630

    merge with optimize app.js

--- resources/js/components/nursingSearch.vue
+++ resources/js/components/nursingSearch.vue
@@ -357,7 +357,7 @@
                     <option data-price-type="" value="10">10万円以下</option>
                     <option data-price-type="" value="12">12万円以下</option>
                     <option data-price-type="" value="14">14万円以下</option>
-                    <option data-price-type="" value="16">16万円以下</option>     
+                    <option data-price-type="" value="16">16万円以下</option>
                     <option data-price-type="" value="18">18万円以下</option>
                     <option data-price-type="" value="20">20万円以下</option>
                     <option data-price-type="" value="22">22万円以下</option>
@@ -385,27 +385,27 @@
 
         <section id="holder" >
         <div class="row" >
-          <div class="col-sm-12 col-md-12">    
-            <div v-if="loading" class=" m-t-10 m-b-10" style="background-color:gray;opacity:0.9;position:relative;z-index:10;">   
+          <div class="col-sm-12 col-md-12">
+            <div v-if="loading" class=" m-t-10 m-b-10" style="background-color:gray;opacity:0.9;position:relative;z-index:10;">
                <div class="lds-ripple m-t-10 m-b-10" >
                   <div></div><div></div>
-                </div>           
+                </div>
                 <div class="col-12 overlay" style="z-index:9">
-                 
+
                 </div>
             </div>
             <div v-if="!loading" class="m-t-10 m-b-10">
               <div id="mymap"></div>
-            </div>     
-              <!-- <div id="mymap" class="select m-t-10 m-b-10"></div> -->       
+            </div>
+              <!-- <div id="mymap" class="select m-t-10 m-b-10"></div> -->
           </div>
         </div>
         </section>
-        
+
 
         <!-- nursing list -->
        <div id="nursing-search" >
-           <span v-if="norecord_msg">
+           <span v-if="norecord_msg && loading==false">
                 <div class="container-fuid m-t-20">
                     <p class="nosearch-icon">
                         <svg x="0px" y="0px" width="30" height="30" viewBox="0 0 172 172" style=" fill:red;"><g transform=""><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><path d="M86,172c-47.49649,0 -86,-38.50351 -86,-86v0c0,-47.49649 38.50351,-86 86,-86v0c47.49649,0 86,38.50351 86,86v0c0,47.49649 -38.50351,86 -86,86z" fill="none"></path><path d="M86,168.56c-45.59663,0 -82.56,-36.96337 -82.56,-82.56v0c0,-45.59663 36.96337,-82.56 82.56,-82.56v0c45.59663,0 82.56,36.96337 82.56,82.56v0c0,45.59663 -36.96337,82.56 -82.56,82.56z" fill="none"></path><path d="M0,172v-172h172v172z" fill="none"></path><path d="M3.44,168.56v-165.12h165.12v165.12z" fill="none"></path><path d="M86,172c-47.49649,0 -86,-38.50351 -86,-86v0c0,-47.49649 38.50351,-86 86,-86v0c47.49649,0 86,38.50351 86,86v0c0,47.49649 -38.50351,86 -86,86z" fill="none"></path><path d="M86,168.56c-45.59663,0 -82.56,-36.96337 -82.56,-82.56v0c0,-45.59663 36.96337,-82.56 82.56,-82.56v0c45.59663,0 82.56,36.96337 82.56,82.56v0c0,45.59663 -36.96337,82.56 -82.56,82.56z" fill="none"></path><path d="M0,172v-172h172v172z" fill="none"></path><path d="M3.44,168.56v-165.12h165.12v165.12z" fill="none"></path><path d="M86,172c-47.49649,0 -86,-38.50351 -86,-86v0c0,-47.49649 38.50351,-86 86,-86v0c47.49649,0 86,38.50351 86,86v0c0,47.49649 -38.50351,86 -86,86z" fill="none"></path><path d="M86,168.56c-45.59663,0 -82.56,-36.96337 -82.56,-82.56v0c0,-45.59663 36.96337,-82.56 82.56,-82.56v0c45.59663,0 82.56,36.96337 82.56,82.56v0c0,45.59663 -36.96337,82.56 -82.56,82.56z" fill="none"></path><g fill="#666666"><path d="M74.53333,17.2c-31.59643,0 -57.33333,25.73692 -57.33333,57.33333c0,31.59641 25.7369,57.33333 57.33333,57.33333c13.73998,0 26.35834,-4.87915 36.24766,-12.97839l34.23203,34.23203c1.43802,1.49778 3.5734,2.10113 5.5826,1.57735c2.0092,-0.52378 3.57826,-2.09284 4.10204,-4.10204c0.52378,-2.0092 -0.07957,-4.14458 -1.57735,-5.5826l-34.23203,-34.23203c8.09923,-9.88932 12.97839,-22.50768 12.97839,-36.24766c0,-31.59641 -25.7369,-57.33333 -57.33333,-57.33333zM74.53333,28.66667c25.39939,0 45.86667,20.46729 45.86667,45.86667c0,25.39937 -20.46728,45.86667 -45.86667,45.86667c-25.39939,0 -45.86667,-20.46729 -45.86667,-45.86667c0,-25.39937 20.46728,-45.86667 45.86667,-45.86667zM91.67734,51.52161c-1.51229,0.03575 -2.94918,0.66766 -3.99765,1.75807l-13.14636,13.14636l-13.14636,-13.14636c-1.07942,-1.10959 -2.56162,-1.73559 -4.10963,-1.73568c-2.33303,0.00061 -4.43306,1.41473 -5.31096,3.57628c-0.8779,2.16155 -0.3586,4.6395 1.31331,6.26669l13.14636,13.14636l-13.14636,13.14636c-1.49777,1.43802 -2.10111,3.5734 -1.57733,5.58259c0.52378,2.0092 2.09283,3.57825 4.10203,4.10203c2.0092,0.52378 4.14457,-0.07956 5.58259,-1.57733l13.14636,-13.14636l13.14636,13.14636c1.43802,1.49778 3.5734,2.10113 5.5826,1.57735c2.0092,-0.52378 3.57826,-2.09284 4.10204,-4.10204c0.52378,-2.0092 -0.07957,-4.14458 -1.57735,-5.5826l-13.14636,-13.14636l13.14636,-13.14636c1.70419,-1.63875 2.22781,-4.1555 1.31865,-6.33798c-0.90916,-2.18248 -3.06468,-3.58317 -5.42829,-3.52739z"></path></g></g></g></svg>
@@ -415,7 +415,7 @@
                 </div>
             </span>
         <div class="row" v-if="nus_data.length > 0">
-          <div class="card-carousel-wrapper col-12">
+          <div class="card-carousel-wrapper col-12" v-if="loading==false">
             <div class="nav-box" @click="moveCarousel(-1)" :disabled="atHeadOfList">
               <div class="nav-content mr-2">
                   <div class="card-carousel--nav__left"></div>
@@ -626,7 +626,7 @@
       <!--end search list-->
 
         <div class=" col-12">
-            <div class="row">
+            <div class="row" v-if="loading==false">
                 <div id="job_detail" class="col-md-6 col-sm-12" style="margin-top:20px;" v-for="(nus,index) in displayItems" :key="nus.id">
                     <div class="nur-content">
                     <div class="job-header">
@@ -1002,7 +1002,7 @@ searchfreeword(){
               console.log(response)
             if(response.data.nursing.length > 0)
             {
-             
+
                 $("#mymap").css("display", "block");
                 $("#filtertable").css("display", "block");
                 $("#nursing-search").css("display", "block");
@@ -1098,8 +1098,8 @@ getStateClick(e) {
                 },
             })
                 .then((response) => {
-                 $("#mymap").css({'display' : 'block','height' : '500px','width':'100%'});  
-                 // $("#mymap").css('display', 'block');               
+                 $("#mymap").css({'display' : 'block','height' : '500px','width':'100%'});
+                 // $("#mymap").css('display', 'block');
                   $("#nursing-search").css("display", "block");
                   $("#filtertable").css("display", "block");
 
@@ -1159,7 +1159,7 @@ nursingSearchData(index){
 
 
 
-        
+
         },
 // map change dropdown function
 // make infowindow, marker , google map
@@ -1454,13 +1454,13 @@ changeMap(response){
 
                     if(this.markers.length > 0 )
                     {
-                      
+
                         this.coordinates(theCity,lat,lng);
 
                         this.infoWindow(item, mmarker);
                     }
                     else{
-                      
+
                         this.coordinates(theCity,lat,lng);
                     }
                 }
@@ -1595,12 +1595,12 @@ search(){
               }
             this.show_paginate = true;
             });
-        
+
 
         },
         // hover animate function
         mouseover(index) {
-          
+
             for (let i = 0; i < this.markerHover.length; i++) {
 
                 if(this.markers[i]['alphabet'] == index)
@@ -1749,12 +1749,12 @@ search(){
   width: 80px;
   height: 80px;
   top: 40%;
-  left: 50%; 
+  left: 50%;
   z-index: 1; */
   position: relative;
   width: 100% !important;
   height: 440px !important;
- 
+
 }
 .lds-ripple div {
   position: absolute;
@@ -1792,7 +1792,7 @@ search(){
   background-color: rgba(0, 0, 0, 0.19);
   opacity: 0.1; */
   top: 0;
-  background-color: rgba(0, 0, 0, 0.19);  
+  background-color: rgba(0, 0, 0, 0.19);
   position: absolute;
   background: #d2d2d2;
   width: 100%;
