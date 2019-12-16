c25bcfe20d4a90c5bb59ac67b3ec8babb3a8d5e9
EiM0hM0hAung <eimoh.aidma@gmail.com>
2019-12-16 14:46:35 +0630

    job apply and detail for responsive

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -21,7 +21,7 @@
                                 </div>
                                     <!--end slider for ads-->
                                 <div class="row justify-content-md-center">
-                                    <div class="col-12 col-lg-12  tab">
+                                    <div class="col-12 col-lg-12  tab p0-480">
                                     <!-- vue component -->
                                         <router-view :key="$route.fullPath"></router-view>
                                     </div>
@@ -69,13 +69,11 @@
                             <div class="container-fluid main-wrapper">
                                 <!--slider for ads-->
                                 <div class="col-md-auto pad-free">
-                                <div class="row col-md-12 pad-free fixed-ads-fluid" style="hight:200px">
-                                        <ads_slider></ads_slider>
-                                </div>
+                                    <ads_slider></ads_slider>
                                 </div>
                                     <!--end slider for ads-->
                                 <div class="row justify-content-md-center">
-                                    <div class="col-12 col-lg-12  tab">
+                                    <div class="col-12 col-lg-12  tab  p0-480">
                                     <!-- vue component -->
                                         <router-view :key="$route.fullPath"></router-view>
                                 </div>
