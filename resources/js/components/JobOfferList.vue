2290fcd30dadc81ab7f5af00399b2842542fed97
sus547983 <susandiaung565@gmail.com>
2019-12-16 08:41:02 +0630

    job

--- resources/js/components/JobOfferList.vue
+++ resources/js/components/JobOfferList.vue
@@ -263,9 +263,13 @@
             },
             methods: {
                    getAllJobs() {
+                    this.$loading(true);
                     this.axios.get("/api/job/index").then(response => {
-                        console.log(response.data)
+                        //   this.$loading(false);
+                      
                         this.jobs = response.data.profilejob;
+                         console.log("this");
+                         console.log(this.jobs);
                         this.customer_id = response.data.user;
                         if (this.jobs.length > this.size) {
                         this.pagination = true;
