2a15cb57aa26a8fb5aac1403dfe8e832bed0be27
zinkomyint <zinkomyint.gamer@gmail.com>
2020-01-07 15:04:19 +0630

    customerlist

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -154,7 +154,9 @@ class CustomerController extends Controller
             $hospital->delete();
         }
         
-        return response()->json('Customer successfully deleted');
+        $customers = Customer::all();
+        $data = array("status"=>"deleted", "customers"=>$customers);
+        return response()->json($data);
     }
 
     public function confirm($id)
@@ -214,7 +216,9 @@ class CustomerController extends Controller
             $cus->user_id = $lastid;
             $cus->save();
 
-            return response()->json('success');
+            $customers = Customer::all();
+            $data = array("status"=>"success", "customers"=>$customers);
+            return response()->json($data);
         }
     }
 
