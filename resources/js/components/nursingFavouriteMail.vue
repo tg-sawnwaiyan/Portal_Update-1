34d514ab4cc2e8bab352cad52003bf83d6c4a08c
ThuzarTS <thuzar.ts92@gmail.com>
2020-01-08 08:00:35 +0630

    fav error msg edit

--- resources/js/components/nursingFavouriteMail.vue
+++ resources/js/components/nursingFavouriteMail.vue
@@ -45,10 +45,16 @@
                         <div class="form-group m-0 row bd">
                             <div class="col-md-3 col-sm-12 form-left"> <label>ふりがな <span class="error sp1">必須</span></label></div>
                             <div class="col-md-9 col-sm-12 form-right">
-                                <input type="text" id="furigana" name="furigana" class="form-control float-left" placeholder="ふりがなを入力してください。" v-model="comments.furigana" @keyup="ChekChar" @change="ChekChar" @focusout="focusFuri"/>
-                                <span class="error m-l-30" v-if="furigana_focus">※入力は必須です。</span>
-                                <span class="float-left eg-txt"> 例）さがし たろう</span>
-                                <div class="text-danger mt-2 ml-4 char-err"></div>
+                                <div class="col-md-12 pad-free">   
+                                    <!-- <div class="col-md-9 pad-free"> -->
+                                        <input type="text" id="furigana" name="furigana" class="form-control float-left" placeholder="ふりがなを入力してください。" v-model="comments.furigana" @keyup="ChekChar" @change="ChekChar" @focusout="focusFuri"/>
+                                    <!-- </div>
+                                    <div class="col-md-3"> -->
+                                        <span class="error m-l-30" v-if="furigana_focus">※入力は必須です。</span>
+                                        <span class="float-left eg-txt"> 例）さがし たろう</span>
+                                    <!-- </div>                                     -->
+                                </div>                                
+                                <div class="col-md-12 text-danger mt-2 ml-4 char-err"></div>
                             </div>
                         </div>
                         <div class="form-group m-0 row bd">
