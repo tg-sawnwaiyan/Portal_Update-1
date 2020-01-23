930276f0f0339fccf3c493b83898829f37130fd5
ThuzarTS <thuzar.ts92@gmail.com>
2020-01-23 13:14:23 +0630

    server update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="user" content="Auth::user()">
 <title>{{ config('app.name', 'Healthcare Portal') }}</title>
-<script src="{{ asset('js/manifest.js') }}" defer></script>
+<!-- <script src="{{ asset('js/manifest.js') }}" defer></script> -->
 <!-- <script src="{{ asset('js/vendor.js') }}" defer></script> -->
 <script src="{{ asset('js/app.js') }}" defer></script>
 
