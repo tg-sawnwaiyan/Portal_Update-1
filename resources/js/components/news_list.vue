7c32edeaf5fef435e24ea2a373c0e13144f94860
EiM0hM0hAung <eimoh.aidma@gmail.com>
2020-02-06 13:25:51 +0630

    create news related card search width adjust for sp

--- resources/js/components/news_list.vue
+++ resources/js/components/news_list.vue
@@ -1,7 +1,6 @@
 <template>
     <!-- Page Content  -->
-    <div>
-        
+    <div id="news_list">       
         <div class="col-12  tab-content">
             <div class="p-2 p0-480">
                 <div v-if="norecord_msg" class="card card-default card-wrap">
@@ -16,15 +15,15 @@
                     </a>
                 </div>
                 <div v-else class="container-fuid">
-                    <h4 class="main-color m-b-10">ニュース検索</h4>
+                    <h4 class="main-color mb-3">ニュース検索</h4>
                     <div class="row">
-                        <div class="col-12 col-sm-6 mb-3">
+                        <div class="col-12 col-sm-6 mb-2">
                             <input type="text" class="form-control w-75 w-sm-100" placeholder="ニュース検索" id="search-item" @keyup="searchbyCategory()" />
                         </div>
                         <div class="col-12 col-sm-6">
                             <div class=" d-flex  justify-content-md-end align-items-center">
-                            <label for="selectBox" class="w-sm-25">カテゴリー</label>
-                            <select  id="selectBox" class="form-control w-65 w-sm-75 ml-2" @change="searchbyCategory()">
+                            <label for="selectBox" class="select_label  mr-2">カテゴリー</label>
+                            <select  id="selectBox" class="form-control select_box" @change="searchbyCategory()">
                                     <option selected="selected" value>全体</option>
                                     <option v-for="category in categories" :key="category.id" v-bind:value="category.id">{{category.name}}</option>
                                 </select>
@@ -36,7 +35,7 @@
                         <h5>ニュース一覧</h5>
                         <div class="ml-auto" v-if="!norecord_msg">
                             <router-link to="/create_news" class="main-bg-color create-btn all-btn">
-                                <i class="fas fa-plus-circle"></i> <span>ニュース</span>新規作成
+                                <i class="fas fa-plus-circle"></i> <span class="first_txt">ニュース</span><span>新規作成</span>
                             </router-link>
                         </div>
                     </div>
