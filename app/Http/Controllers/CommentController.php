45a36c561c0db33df5fcdf7bb464344a4534c58a
ayethandar <ayethandar@management-partners.co.jp>
2020-02-07 14:52:20 +0630

    info alert

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -35,7 +35,7 @@ class CommentController extends Controller
             ->where('nursing_profiles.recordstatus', 1)
             ->where('comments.type','nursing')
             ->orderBy('comments.id','DESC')
-            ->paginate(1);
+            ->paginate(12);
 
             $query = "select id,name from nursing_profiles where recordstatus = 1 and name is not null";
             $profilelist = DB::select($query);
@@ -47,7 +47,7 @@ class CommentController extends Controller
             ->where('hospital_profiles.recordstatus', 1)
             ->where('comments.type','hospital')
             ->orderBy('comments.id','DESC')
-            ->paginate(1);
+            ->paginate(12);
 
             $query = "select id,name from hospital_profiles where recordstatus = 1 and name is not null";
             $profilelist = DB::select($query);
@@ -78,7 +78,7 @@ class CommentController extends Controller
                 ->where('nursing_profiles.recordstatus', 1)
                 ->where('comments.type','nursing')
                 ->orderBy('comments.id','DESC')
-                ->paginate(1);
+                ->paginate(12);
             }
             else{
                 $commentList = DB::table('comments')
@@ -88,7 +88,7 @@ class CommentController extends Controller
                 ->where('comments.type','nursing')
                 ->where('comments.profile_id',$profileid)
                 ->orderBy('comments.id','DESC')
-                ->paginate(1);
+                ->paginate(12);
             }
            
         }
@@ -101,7 +101,7 @@ class CommentController extends Controller
                 ->where('hospital_profiles.recordstatus', 1)
                 ->where('comments.type','hospital')
                 ->orderBy('comments.id','DESC')
-                ->paginate(1);
+                ->paginate(12);
             }
             else{
                 $commentList = DB::table('comments')
@@ -111,7 +111,7 @@ class CommentController extends Controller
                 ->where('comments.type','hospital')
                 ->where('comments.profile_id',$profileid)
                 ->orderBy('comments.id','DESC')
-                ->paginate(1);
+                ->paginate(12);
             }
            
         
