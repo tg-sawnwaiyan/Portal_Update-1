e7d11654943c54d85700f7e531d365da0e3e91c2
zinkomyint <zinkomyint.gamer@gmail.com>
2020-02-11 08:10:33 +0630

    mail_blade_all

--- resources/views/mail/resetMail.blade.php
+++ resources/views/mail/resetMail.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -46,4 +46,62 @@
     </div>
 </body>
 
-</html>
+</html> -->
+
+
+<div style="width:800px;">
+<div style="text-align: center;">
+	        <div style="padding: 60px;">
+
+                <!-- <span><span style="font-size: 40px;font-weight: bold;color: #fff;font-family: ;background: #2980B9;border-radius: 4px;padding: 6px;">T-IS</span></span> -->
+                
+                <span style="background: #fbaa84;">               
+                    <a href="#" style="background: #2980B9;font-weight: 900; letter-spacing: 2px; font-size: 16px; mso-height-rule: exactly; line-height: 40px; text-align: center; text-decoration: none; display: block; font-weight: 900; text-transform: uppercase;float:right;padding:7px;">
+                    <span style="color:#ffffff">T-IS</span>
+                    </a> 
+                    <br>          
+                </span>
+	            
+                <h5 style="color:#3e4348;font-size: 12px;margin-bottom: 30px;font-weight: bold;text-align: left;">
+                 【 Healthcare Portal | パスワードの再設定 】
+                                  
+	            </h5>
+                <p style="text-align: left;margin: 0px;">
+                ***********************************************************************************************************************************</p>
+                <p style="text-align:left;margin:2px;color:#3e4348;font-size: 16px;"><span style="color:#d2571c">◎</span>【パスワードの再設定】</p>
+                <p style="text-align:left;margin:2px;color:#3e4348;font-size: 12px;">
+                  以下のボタンをクリックし、パスワードの再設定手続きにお進みください。
+               
+                </p> 
+                <br>
+
+                 <span style="background: #fbaa84;">               
+                    <a href="http://testikportal.management-partners.co.jp/resetpassword?code={{$checkmail[0]['role']}}" style="-moz-border-radius: 4px;background: #fbaa84; border: 1px solid #ff9563; font-weight: 900; letter-spacing: 2px; font-size: 16px; mso-height-rule: exactly; line-height: 40px; text-align: center; text-decoration: none; display: block; border-radius: 4px; font-weight: 900; text-transform: uppercase">
+                    <span style="color:#ffffff">パスワードを再設定する</span>
+                    </a> 
+                    <br>          
+                </span>
+
+          
+                <p style="text-align:left;margin:2px;color:#3e4348;font-size: 12px;">
+                ※再設定しない場合は、不要このメッセージを無視してください。
+                <br> <br>
+                ※パスワードは、上記から変更操作をしない限り変更されません。
+                <br> <br>
+                ※再設定用URLは24時間有効です。
+                               
+                </p><br>
+                <p style="text-align: left;margin: 0px;">
+                ***********************************************************************************************************************************</p>   
+                <p style="font-size:12px;color:#807c7c;text-align:left;"> 
+                    <span style="color:#000">[T-IS ティーズ]</span>
+                    <br/>
+                    〒163-0713
+                    <br/>                        
+                    東京都新宿区西新宿2-7-1小田急第一生命ビル13階 
+                    <br/>                       
+                    TEL：03-6911-???? FAX：03-6911-????
+                </p>	          
+	        </div>
+	    </div>
+</div>
