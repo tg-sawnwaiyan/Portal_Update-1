4149f5113a58b0cc3aac8bcd8b160631f6a5f236
mawutmoneoo <mawutmoneoo@gmail.com>
2020-02-25 14:47:40 +0630

    nursing profile

--- resources/js/components/NursingProfile.vue
+++ resources/js/components/NursingProfile.vue
@@ -523,7 +523,7 @@
 
                                 <div class="col-md-10 float-right special-feature-toggle-div toggle-div m-t-10">
                                     <div class="row">
-                                        <div v-for="feat in feature_list" :key="feat.id" class="form-check form-check-inline col-6 col-md-3">
+                                        <div v-for="feat in feature_list" :key="feat.id" class="form-check form-check-inline col-6 col-md-4 col-lg-3">
                                             <label  class="form-check-label control control--checkbox" style="padding-left:5px;">
                                                 <input type="checkbox" class="form-check-input"  name="special-features" v-bind:value="feat.id"  v-model="feat.checked">
                                                 {{feat.name}}
