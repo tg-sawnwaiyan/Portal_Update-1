be573d606ecfc65ddd76277ef795e0efe0b03585
ThuzarTS <thuzar.ts92@gmail.com>
2020-03-03 15:27:54 +0630

    deleteSentMail subject

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -27,7 +27,7 @@ class CustomerController extends Controller
     }
     public function nusaccount($id) {
         $nuscustomer = NursingProfile::where(['nursing_profiles.customer_id'=>$id])->select('id','logo','name','phone','email')->get();
-        $status = NursingProfile::join('customers','customers.id','=','nursing_profiles.customer_id')->where(['customers.recordstatus'=>2])->count();
+        $status = NursingProfile::join('customers','customers.id','=','nursing_profiles.customer_id')->where(['customers.recordstatus'=>1])->count();
         return response()->json(array("nuscustomer"=>$nuscustomer,"status"=>$status));
     }
     public function hosaccount($id) {
