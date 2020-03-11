807201edb7aaea8717006cbe79b17baab13b5048
sus547983 <susandiaung565@gmail.com>
2020-03-11 14:02:10 +0630

    merge with dev

--- resources/js/components/customerlist.vue
+++ resources/js/components/customerlist.vue
@@ -20,7 +20,7 @@
                      <div class="row mb-4">
                         <div class="col-md-12">
                               <select  v-model="status" id="selectBox" class="form-control select_box" @change="searchCustomer()">
-                                    <option selected="selected" value>choose item</option>
+                                    <option selected="selected" value>選択してください</option>
                                     <option value="1"> Activate </option>
                                     <option value="0"> Deactivate </option>
                                     <option value="2"> Pending </option>
