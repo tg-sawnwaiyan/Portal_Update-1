08b0f88bbcdbb5dddda6eff8b1370e96d434c1c5
EiM0hM0hAung <eimoh.aidma@gmail.com>
2020-03-12 11:13:12 +0630

    nursing mail blades

--- resources/views/mail/adminNursingMail.blade.php
+++ resources/views/mail/adminNursingMail.blade.php
@@ -1,22 +1,16 @@
 <pre>
 TIS ティーズ管理者 様
 
-TIS ティーズへ資料請求がありました
-                 
-****************************************
-                
-◎【資料請求先】
+TIS ティーズへ資料請求がありました。
 
+【資料請求先】
 @foreach($getComment->fav_name_copy as $name)
 @if($name != null)
 {{$name}}
 @endif
 @endforeach
-
-****************************************
                 
-◎【資料請求された方について】
-               
+【資料請求された方について】               
 お名前：{{$getComment->name}}             
 フリガナ：{{$getComment->furigana}}                
 生年月日：{{$getComment->bdate}}               
@@ -26,28 +20,20 @@ TIS ティーズへ資料請求がありました
 電話番号：{{$getComment->phone}}               
 メールアドレス：{{$getComment->mail}}
 
-◎【入居対象者様について】
-           
+【入居対象者様について】           
 入居対象者様とのご関係：{{$getComment->relation}}               
 お名前：{{$getComment->ttname}}              
 性別：{{$getComment->sex}}                
 年齢：{{$getComment->years}}               
 介護度：{{$getComment->nursing}}              
-認知症：{{$getComment->fect}}               
+認知症：{{$getComment->fect}}
+
 ご要望や、お困りごと、その他お問い合わせ：{{$getComment->hope}}                    
               
-****************************************
-             
+************************************************            
 TIS ティーズ                 
-
 運営会社　株式会社 TRUST-ESTATE               
 〒164-0003　東京都中野区東中野1-59-9　リックビル2階                                         
-TEL：03-6279-1951   FAX：03-6279-1952
-
+TEL ：03-6279-1951   FAX ：03-6279-1952
+************************************************
 </pre>
-
-
-  
-
-
-
--- resources/views/mail/adminNursingMailplaintext.blade.php
+++ resources/views/mail/adminNursingMailplaintext.blade.php
@@ -1,21 +1,16 @@
+<pre>
 TIS ティーズ管理者 様
 
-TIS ティーズへ資料請求がありました                 
-	           
-****************************************
-                
-◎【資料請求先】
+TIS ティーズへ資料請求がありました。
 
+【資料請求先】
 @foreach($getComment->fav_name_copy as $name)
 @if($name != null)
 {{$name}}
 @endif
 @endforeach
-
-****************************************
                 
-◎【資料請求された方について】
-               
+【資料請求された方について】               
 お名前：{{$getComment->name}}             
 フリガナ：{{$getComment->furigana}}                
 生年月日：{{$getComment->bdate}}               
@@ -23,30 +18,22 @@ TIS ティーズへ資料請求がありました
 郵便番号：{{$getComment->postal}}               
 都道府県、市区町村、番地（建物名)：{{$getComment->division}}{{$getComment->townshipname}}{{$getComment->city}}              
 電話番号：{{$getComment->phone}}               
-メールアドレス：{{$getComment->mail}} 
+メールアドレス：{{$getComment->mail}}
 
-◎【入居対象者様について】
-           
+【入居対象者様について】           
 入居対象者様とのご関係：{{$getComment->relation}}               
 お名前：{{$getComment->ttname}}              
 性別：{{$getComment->sex}}                
 年齢：{{$getComment->years}}               
 介護度：{{$getComment->nursing}}              
 認知症：{{$getComment->fect}}
-               
+
 ご要望や、お困りごと、その他お問い合わせ：{{$getComment->hope}}                    
               
-****************************************
-             
-TIS ティーズ
-
+************************************************            
+TIS ティーズ                 
 運営会社　株式会社 TRUST-ESTATE               
 〒164-0003　東京都中野区東中野1-59-9　リックビル2階                                         
-TEL：03-6279-1951   FAX：03-6279-1952
-
-
-
-  
-
-
-
+TEL ：03-6279-1951   FAX ：03-6279-1952
+************************************************
+</pre>
