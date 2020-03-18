01821072b5cd84a9abef9d434215a04baa573beb
zinkomyint <zinkomyint.gamer@gmail.com>
2020-03-18 16:00:27 +0630

    nur-profile

--- resources/js/components/FeatureList.vue
+++ resources/js/components/FeatureList.vue
@@ -181,13 +181,13 @@
                                     if(error.response.status == 404){
                                         // this.$swal("このカテゴリーに関連するニュースがあるため、削除できません。");
                                         this.$swal({
-                                            title: "削除に失敗しました",
-                                            text: "削除に失敗しました この特長の施設が存在するため削除できません ",
+                                            // title: "削除に失敗しました",
+                                            html: "削除に失敗しました。<br> この特長の施設が存在するため削除できません。 ",
                                             type: "error",
                                             width: 350,
                                             height: 200,
                                             confirmButtonText: "閉じる",
-                                            confirmButtonColor: "#dc3545",
+                                            confirmButtonColor: "#ff5462",
                                             allowOutsideClick: false,
                                         });
                                     }
