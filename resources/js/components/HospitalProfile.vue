6ff79d58fd41e4db23a0a5ebb980af9961e7fdb3
sus547983 <susandiaung565@gmail.com>
2020-03-20 11:16:07 +0630

    remove texteditor placeholder for create_news

--- resources/js/components/HospitalProfile.vue
+++ resources/js/components/HospitalProfile.vue
@@ -1089,7 +1089,7 @@ export default {
                         toolbar: [
                         ['bold', 'italic', 'underline', 'strike'],
                         ['blockquote', 'code-block'],
-                        [{ 'header': 1 }, { 'header': 2 },{ 'header': 1 }],
+                        [{ 'header': 1 }, { 'header': 2 }],
                         [{ 'list': 'ordered' }, { 'list': 'bullet' }],
                         [{ 'script': 'sub' }, { 'script': 'super' }],
                         [{ 'indent': '-1' }, { 'indent': '+1' }],
