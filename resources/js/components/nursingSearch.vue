7c7003d6fa265657cd243b9afbb03cecc2151916
EiM0hM0hAung <eimoh.aidma@gmail.com>
2020-03-23 16:57:35 +0630

    css minimize

--- resources/js/components/nursingSearch.vue
+++ resources/js/components/nursingSearch.vue
@@ -1855,7 +1855,7 @@ div#holder {
     transition: all .2s ease-in-out;
 }
 .card_1:hover {   
-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);   
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);   
 }
 .image {
     height: 200px;
