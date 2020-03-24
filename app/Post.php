ea3c29c3eeab0258afe09b495a46880157f60694
EiM0hM0hAung <eimoh.aidma@gmail.com>
2020-03-24 17:03:54 +0630

    breaking news

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'title','photo','main_point','body','category_id', 'related_news' ,'user_id','recordstatus'
+        'title','photo','main_point','body','category_id', 'related_news' ,'user_id','recordstatus','from_date','to_date'
     ];
 }
