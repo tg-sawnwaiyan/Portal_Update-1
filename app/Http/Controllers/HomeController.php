a1e1d18f1450d62cf7e568203a304d5c681f8eee
Thuzar-TS <thuzar.ts92@gmail.com>
2020-03-30 11:04:18 +0630

    testing to date null in PR create

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -134,7 +134,7 @@ class HomeController extends Controller
         foreach ($list as $li) {
             if($li->to_date == '0000-00-00 00:00:00' || $li->to_date == null)
             {
-                $query = "SELECT * from posts where (category_id = 26 and (from_date <= '".$getTime."' and to_date = '0000-00-00 00:00:00')) limit 16 ";
+                $query = "SELECT * from posts where (category_id = 26 and (from_date <= '".$getTime."' and (to_date = '0000-00-00 00:00:00' or to_date=null))) limit 16 ";
                 $from_date = DB::select($query);
             }
             if($li->to_date != '0000-00-00 00:00:00' && $li->to_date != null){
